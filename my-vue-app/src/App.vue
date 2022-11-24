2d66db30ef6219fda3bf3a91da97035bc80e6e8e
Spong3ibob <leonardomaran17@gmail.com>
2022-11-24 16:41:23 +0100

    aggiunto header main footer

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,35 +1,30 @@
 <script>
-import AppTitle from './components/AppTitle.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   components: {
-    AppTitle
+    AppHeader,
+    AppMain,
+    AppFooter
   }
 }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <AppTitle />
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+    <AppMain/>
+  </main>
+  <footer>
+    <AppFooter/>
+  </footer>
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+  @import "./global.scss"
 </style>
---
+++ my-vue-app/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title:"Header"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<style scoped>
+h1{
+    color: coral;
+}
+</style>
