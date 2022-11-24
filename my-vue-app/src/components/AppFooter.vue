2bfe9b63bb619aeea01e0bdabd9fd40015644226
Spong3ibob <leonardomaran17@gmail.com>
2022-11-24 18:30:18 +0100

    completato header footer

--- my-vue-app/src/components/AppFooter.vue
+++ my-vue-app/src/components/AppFooter.vue
@@ -1,19 +1,49 @@
 <script>
+import HeaderFooter from "../components/HeaderFooter.vue";
+import MainFooter from  "../components/MainFooter.vue";
+import FooterButtom from  "../components/FooterButtom.vue";
+
 export default {
+  components:{
+    HeaderFooter,
+    MainFooter,
+    FooterButtom
+  },
   data() {
     return {
-      title:"Footer"
+
     }
   }
 }
 </script>
 
 <template>
-    <h1>{{title}}</h1>
+  <header>
+   <HeaderFooter/> 
+  </header>
+  <main>
+    <MainFooter/>
+  </main>
+  <footer>
+    <FooterButtom/>
+  </footer>
+   
 </template>
 
-<style >
-h1{
-    color: coral;
+<style lang="scss" scoped>
+header{
+  height: 160px;
+  width: 100%;
+  background-color: var(--primary-color);
+}
+main{
+  height: 370px;
+  width: 100%;
+  background-image: url(../assets/img/footer-bg.jpg);
+}
+footer{
+  height: 100px;
+  width: 100%;
+  background-color: var(--footer-buttom);
 }
 </style>
---
+++ my-vue-app/src/components/HeaderFooter.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+   <nav>
+        <li>
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+            <p>DIGITAL COMICS</p>
+        </li>
+        <li>
+            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+            <p>DC MERCHANDISE</p>
+        </li>
+        <li>
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+            <p>SUBSCRIPTION</p>
+        </li>
+        <li>
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+            <p>COMIC SHOP LOCATOR</p>
+        </li>
+        <li>
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+            <p>DC POWER VISA</p>
+        </li>
+   </nav>
+</template>
+
+<style lang="scss">
+li{
+    display: flex;
+    align-items: center;
+    img{
+        height: 5px;
+    }
+    p{
+        color: white;
+        padding: 0 15px;
+    }
+}
+</style>
